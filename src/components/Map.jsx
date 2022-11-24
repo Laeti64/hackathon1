81a095870fb8be05ef9baeb035eed04e65772c95
Mika6413 <93663744+Mika6413@users.noreply.github.com>
2022-11-24 21:56:45 +0100

    trotinettes

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -8,6 +8,7 @@ import {
 import { poi } from "../utils/axiosTool";
 import calendar from "../assets/calendar.png";
 import station from "../assets/fuel.png";
+import trotinettes from "../assets/trotinettes.png";
 import mapStyles from "./mapStyles";
 import InfoWindoDetails from "./InfoWindoDetails";
 
@@ -19,89 +20,112 @@ function Map() {
   const [events, setEvents] = useState([]);
   const [stations, setStations] = useState([]);
   const [selectedPoi, setSelectedPoi] = useState(null);
+  const [trotinettes, setTrotinettes] = useState([]);
 
   const center = useMemo(() => ({ lat: 44.837789, lng: -0.57918 }), []);
 
   useEffect(() => {
     poi.getEvents().then((result) => setEvents(result.records));
     poi.getStations().then((result) => setStations(result.records));
-  }, []);
+    poi.getTrotinettes().then((result) => setTrotinettes(result.records));
 
-  if (!isLoaded || !events || !stations) return <div>Loading...</div>;
+    if (!isLoaded || !events || !stations) return <div>Loading...</div>;
 
-  return (
-    <>
-      <GoogleMap
-        zoom={12}
-        center={center}
-        mapContainerStyle={{
-          height: "70vh",
-          width: "100%",
-        }}
-        options={{ styles: mapStyles }}
-      >
-        {events.map((poi) => (
-          <MarkerF
-            key={poi.recordid}
-            onClick={() => {
-              setSelectedPoi({
-                poi: poi,
-                type: "event",
+    return (
+      <>
+        <GoogleMap
+          zoom={12}
+          center={center}
+          mapContainerStyle={{
+            height: "70vh",
+            width: "100%",
+          }}
+          options={{ styles: mapStyles }}
+        >
+          {events.map((poi) => (
+            <MarkerF
+              key={poi.recordid}
+              onClick={() => {
+                setSelectedPoi({
+                  poi: poi,
+                  type: "event",
+                  lat: poi.fields.location_coordinates[0],
+                  lng: poi.fields.location_coordinates[1],
+                });
+              }}
+              position={{
                 lat: poi.fields.location_coordinates[0],
                 lng: poi.fields.location_coordinates[1],
-              });
-            }}
-            position={{
-              lat: poi.fields.location_coordinates[0],
-              lng: poi.fields.location_coordinates[1],
-            }}
-            icon={{
-              url: calendar,
-              fillColor: "#EB00FF",
-              scale: 5,
-            }}
-          />
-        ))}
+              }}
+              icon={{
+                url: calendar,
+                fillColor: "#EB00FF",
+                scale: 5,
+              }}
+            />
+          ))}
 
-        {stations.map((poi) => (
-          <MarkerF
-            key={poi.recordid}
-            onClick={() => {
-              setSelectedPoi({
-                poi: poi,
-                type: "station",
+          {stations.map((poi) => (
+            <MarkerF
+              key={poi.recordid}
+              onClick={() => {
+                setSelectedPoi({
+                  poi: poi,
+                  type: "station",
+                  lat: poi.fields.geom[0],
+                  lng: poi.fields.geom[1],
+                });
+              }}
+              position={{
                 lat: poi.fields.geom[0],
                 lng: poi.fields.geom[1],
-              });
-            }}
-            position={{
-              lat: poi.fields.geom[0],
-              lng: poi.fields.geom[1],
-            }}
-            icon={{
-              url: station,
-              fillColor: "#EB00FF",
-              scale: 5,
-            }}
-          />
-        ))}
-
-        {selectedPoi && (
-          <InfoWindow
-            onCloseClick={() => {
-              setSelectedPoi(null);
-            }}
-            position={{
-              lat: selectedPoi.lat,
-              lng: selectedPoi.lng,
-            }}
-          >
-            <InfoWindoDetails poi={selectedPoi} />
-          </InfoWindow>
-        )}
-      </GoogleMap>
-    </>
-  );
+              }}
+              icon={{
+                url: station,
+                fillColor: "#EB00FF",
+                scale: 5,
+              }}
+            />
+          ))}
+          {trotinettes.map((poi) => (
+            <MarkerF
+              key={poi.recordid}
+              onClick={() => {
+                setSelectedPoi({
+                  poi: poi,
+                  type: "trotinettes",
+                  lat: poi.fields.geom[0],
+                  lng: poi.fields.geom[1],
+                });
+              }}
+              position={{
+                lat: poi.fields.location_coordinates[0],
+                lng: poi.fields.location_coordinates[1],
+              }}
+              icon={{
+                url: trotinettes,
+                fillColor: "#EB00FF",
+                scale: 5,
+              }}
+            />
+          ))}
+          {selectedPoi && (
+            <InfoWindow
+              onCloseClick={() => {
+                setSelectedPoi(null);
+              }}
+              position={{
+                lat: selectedPoi.lat,
+                lng: selectedPoi.lng,
+              }}
+            >
+              <InfoWindoDetails poi={selectedPoi} />
+            </InfoWindow>
+          )}
+        </GoogleMap>
+      </>
+    );
+  });
 }
 
 export default Map;
--- src/utils/axiosTool.js
+++ src/utils/axiosTool.js
@@ -19,4 +19,10 @@ export const poi = {
         "https://www.data.economie.gouv.fr/api/records/1.0/search/?dataset=prix-carburants-fichier-quotidien-test-ods&q=&facet=prix_maj&facet=prix_nom&facet=com_arm_name&facet=epci_name&facet=dep_name&facet=reg_name&facet=services_service&facet=rupture_nom&facet=rupture_debut&facet=horaires_automate_24_24&refine.prix_maj=2022&refine.com_arm_name=Bordeaux"
       )
     ).data,
+  getTrotinettes: async () =>
+    (
+      await axiosInstance.get(
+        "https://www.data.gouv.fr/fr/datasets/trottinettes-bird-bordeaux"
+      )
+    ).data,
 };
